bf1e994d76771d01e08fc8b83a22c1e4f7e9596f
Chris Kastorff <encryptio@gmail.com>
2011-08-09 14:08:29 -0700

    preliminary changes to support resizing

--- c/main.c
+++ c/main.c
@@ -13,6 +13,26 @@
 #include "image.h"
 #include "archive.h"
 
+int window_width = 800;
+int window_height = 550;
+
+void reshape_gl(void) {
+    glViewport(0,0,window_width,window_height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0,window_width,window_height,0,-1,1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+float fit_factor(float mw, float mh, float w, float h) {
+    float sf = w/mw;
+    if ( sf*mh > h )
+        sf = h/mh;
+    return sf;
+}
+
 int main(int argc, char **argv) {
     //////
     // Set up SDL
@@ -24,9 +44,9 @@ int main(int argc, char **argv) {
 
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
-    SDL_Surface *screen = SDL_SetVideoMode( 640, 480, 32, SDL_OPENGL );
+    SDL_Surface *screen = SDL_SetVideoMode( window_width, window_height, 32, SDL_OPENGL );
     if ( !screen )
-        errx(1, "Couldn't set 640x480 video: %s", SDL_GetError());
+        errx(1, "Couldn't set %dx%d video: %s", window_width, window_height, SDL_GetError());
 
     fprintf(stderr, "SDL Initialized\n");
 
@@ -38,15 +58,9 @@ int main(int argc, char **argv) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    reshape_gl();
+
     glClearColor(0,0,1,0);
-    glViewport(0,0,640,480);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0,640,480,0,-1,1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
     glClear(GL_COLOR_BUFFER_BIT);
 
     fprintf(stderr, "GL Initialized\n");
@@ -74,7 +88,11 @@ int main(int argc, char **argv) {
         printf("timings: load=%dms, cpu=%dms, gl=%dms\n", gl.load_time, gl.cpu_time, gl.gl_time);
         
         printf("drawing\n");
-        image_draw(&gl, 0, 0, 640, 480);
+        float f = fit_factor(gl.w, gl.h, window_width, window_height);
+        float w = gl.w*f;
+        float h = gl.h*f;
+        glClear(GL_COLOR_BUFFER_BIT);
+        image_draw(&gl, (window_width-w)/2, (window_height-h)/2, (window_width-w)/2+w, (window_height-h)/2+h);
 
         image_gl_destroy(&gl);
 
